8fb9a0db86dfbeec99aac5b84b33d6a4003cd4cb
Sreejith S <sreejith.sreekumar@247-inc.com>
2016-10-05 14:18:19 +0530

    add notes for linear, svm and softmax classifiers

---
+++ classifiers/linear_classifier.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+
+# from linear_svm import *
+# from softmax import *
+
+class LinearClassifier():
+    """
+    """
+    
+    def __init__(self):
+        """
+        """
+        self.W = None
